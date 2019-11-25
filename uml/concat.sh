a2b83fa240ac2bdb789f6caba97a06e888798269
HKalbasi <kalbasi@ce.sharif.edu>
2019-11-25 10:21:57 +0330

    append something when concating

--- uml/concat.sh
+++ uml/concat.sh
@@ -4,6 +4,7 @@ cat *.uml > concat.uml
 sed -i '/@startuml/d' concat.uml
 sed -i '/@enduml/d' concat.uml
 sed -i '1 i\@startuml' concat.uml
-echo "@enduml" >> concat.uml
+cat concatEnd >> concat.uml
+printf "\n%s\n" "@enduml" >> concat.uml
 java -jar plantuml.jar concat.uml
 echo done
