f13d20fe13c4941d87591ed7df81e14c87c76fb3
HKalbasi <kalbasi@ce.sharif.edu>
2019-12-17 16:39:20 +0330

    create store class

---
+++ PlantsVsZombies/src/account/Store.java
@@ -0,0 +1,22 @@
+package account;
+
+import java.util.ArrayList;
+
+import creature.Dna;
+import creature.being.plant.PlantDna;
+import creature.being.zombie.ZombieDna;
+
+public class Store {
+  private Account owner;
+  private ArrayList<PlantDna> myPlants;
+  private ArrayList<ZombieDna> myZombies;
+  public int money;
+  public void buyCard(Dna card) {
+    if (card instanceof PlantDna) {
+      myPlants.add((PlantDna)card);
+    }
+    if (card instanceof ZombieDna) {
+      myZombies.add((ZombieDna)card);  
+    }
+  }
+}
