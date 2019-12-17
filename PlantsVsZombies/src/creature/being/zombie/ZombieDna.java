5a55ddc8dbae8b88c15d73eb9ed406afe8dfc49b
HKalbasi <kalbasi@ce.sharif.edu>
2019-12-17 16:46:10 +0330

    add Zombie constructor

--- PlantsVsZombies/src/creature/being/zombie/ZombieDna.java
+++ PlantsVsZombies/src/creature/being/zombie/ZombieDna.java
@@ -2,7 +2,14 @@ package creature.being.zombie;
 
 import java.util.List;
 
-public class ZombieDna {
+import creature.Dna;
+
+public class ZombieDna extends Dna {
+    public ZombieDna(String name) {
+        super(name);
+        // TODO Auto-generated constructor stub
+    }
+
     private static List<ZombieDna> allDnas;
 
     public static List<ZombieDna> getAllDnas() {
