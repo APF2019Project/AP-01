632bd30a7fb3ef89d6770d04fd8d91c196ba61b2
HKalbasi <kalbasi@ce.sharif.edu>
2019-12-17 17:08:58 +0330

    convert array of cards to array of strings

--- PlantsVsZombies/src/account/Store.java
+++ PlantsVsZombies/src/account/Store.java
@@ -10,15 +10,15 @@ import creature.being.zombie.ZombieDna;
 public class Store implements Serializable {
   private static final long serialVersionUID = 5608599943348289833L;
   // private Account owner;
-  private ArrayList<PlantDna> myPlants;
-  private ArrayList<ZombieDna> myZombies;
+  private ArrayList<String> myPlants;
+  private ArrayList<String> myZombies;
   public int money;
   public void buyCard(Dna card) {
     if (card instanceof PlantDna) {
-      myPlants.add((PlantDna)card);
+      myPlants.add(card.getName());
     }
     if (card instanceof ZombieDna) {
-      myZombies.add((ZombieDna)card);  
+      myZombies.add(card.getName());  
     }
   }
 
