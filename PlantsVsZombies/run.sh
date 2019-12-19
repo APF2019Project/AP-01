6d602827f440c806060a6dabc826305965cd32be
HKalbasi <kalbasi@ce.sharif.edu>
2019-12-19 22:31:05 +0330

    add gson library

--- PlantsVsZombies/run.sh
+++ PlantsVsZombies/run.sh
@@ -1,2 +1,5 @@
-javac $(find src -name '*.java') -d ~/.ap-project-dev/
-java -cp ~/.ap-project-dev/ main.Program
+if [ ! javac -cp lib/gson-2.8.6.jar $(find src -name '*.java') -d ~/.ap-project-dev/ ]
+then
+  exit 1
+fi
+java -cp ~/.ap-project-dev/:lib/\* main.Program
--- PlantsVsZombies/src/creature/being/plant/PlantDna.java
+++ PlantsVsZombies/src/creature/being/plant/PlantDna.java
@@ -2,8 +2,15 @@ package creature.being.plant;
 
 import creature.being.BeingDna;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
+
+import com.google.gson.*;
 
 public class PlantDna extends BeingDna {
 
@@ -11,11 +18,28 @@ public class PlantDna extends BeingDna {
 
   public static List<PlantDna> getAllDnas() {
     ArrayList<PlantDna> options = new ArrayList<>();
-      options.add(new PlantDna("tofangi"));
+      options.add(PlantDna.fromJson("{\"name\":\"tofangi\"}"));
       options.add(new PlantDna("bombi"));
       options.add(new PlantDna("khari"));
       options.add(new PlantDna("gol"));
     return options;
+  }
+
+  public static PlantDna fromJson(String json) {
+    Gson gson = new Gson();
+    return gson.fromJson(json, PlantDna.class);
+  }
+
+  public static void loadFromData() {
+    try (Stream<Path> paths = Files.walk(Paths.get("/home/you/Desktop"))) {
+      paths
+        .filter(Files::isRegularFile)
+        .forEach(System.out::println);
+    }
+    catch(IOException e) {
+      e.printStackTrace();
+      System.exit(0);
+    }
   }
 
   private Integer sun = null;
