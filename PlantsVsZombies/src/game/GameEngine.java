1f1022c86021215aa51494b5d75bdfd9c39c56c2
ImaniAm <amirno.2020@gmail.com>
2019-12-29 22:57:29 +0330

    add getAmmunitions to gameEngine

--- PlantsVsZombies/src/game/GameEngine.java
+++ PlantsVsZombies/src/game/GameEngine.java
@@ -189,6 +189,17 @@ public class GameEngine {
         return answer;
     }
 
+    public Plant getAmmunitions(Integer lineNumber, Integer position) {
+        if (locationChecker(lineNumber, position)) return null;
+      //  for (Plant plant : DATABASE.ammunitionPerLine.get(lineNumber))
+        //    if (plant.getLocation().equals(new Location(lineNumber, position)))
+          //      return plant;
+        return null;
+    }
+
+    public Plant getAmmunitions(Location location) {
+        return getAmmunitions(location.lineNumber, location.position);
+    }
     public List<Zombie> getZombies(Location location) {
         return getZombies(location.lineNumber, location.position);
     }
