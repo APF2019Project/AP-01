57a72261aebc69de7d4be221181f864eb0d61d55
HKalbasi <kalbasi@ce.sharif.edu>
2020-01-03 16:29:02 +0330

    init graphic

--- PlantsVsZombies/src/main/Program.java
+++ PlantsVsZombies/src/main/Program.java
@@ -8,29 +8,31 @@ import java.util.Scanner;
 import account.Account;
 import creature.being.plant.PlantDna;
 import creature.being.zombie.ZombieDna;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class Program {
+public class Program extends Application {
 
   public static Scanner scanner;
-  
-  public static void clearScreen() {  
-    System.out.print("\033[H\033[2J");  
-    System.out.flush();  
+
+  public static void clearScreen() {
+    System.out.print("\033[H\033[2J");
+    System.out.flush();
   }
 
   public static String getBackupPath(String file) {
     String workingDirectory;
     String OS = (System.getProperty("os.name")).toUpperCase();
-    if (OS.contains("WIN"))
-    {
+    if (OS.contains("WIN")) {
       workingDirectory = System.getenv("AppData");
-    }
-    else
-    {
+    } else {
       workingDirectory = System.getProperty("user.home");
       workingDirectory += "/Library/Application Support/PVZ4/";
     }
-    return workingDirectory+file;
+    return workingDirectory + file;
   }
 
   public static String streamToString(InputStream is) {
@@ -42,29 +44,31 @@ public class Program {
   }
 
   public static void main(String args[]) {
-    try{
-      scanner = new Scanner(System.in);
+    launch();
+  }
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    Label l = new Label("Loading...");
+    l.setStyle("-fx-font: 3cm arial");
+    Scene scene = new Scene(new StackPane(l), 640, 480);
+    stage.setScene(scene);
+    stage.setFullScreenExitHint("");
+    stage.setFullScreen(true);
+    stage.show();
+    try {
       File mainPath = new File(getBackupPath(""));
-      if (mainPath.exists()){
+      if (mainPath.exists()) {
         Account.recoverAll();
-      }
-      else {
+      } else {
         mainPath.mkdirs();
       }
-      PlantDna.loadFromData(
-        streamToString(
-          Program.class.getResourceAsStream("resource/plantDna.json")
-        )
-      );
-      ZombieDna.loadFromData(
-        streamToString(
-          Program.class.getResourceAsStream("resource/zombieDna.json")
-        )
-      );
-      Pages.loginMenu.action();
+      PlantDna.loadFromData(streamToString(Program.class.getResourceAsStream("resource/plantDna.json")));
+      ZombieDna.loadFromData(streamToString(Program.class.getResourceAsStream("resource/zombieDna.json")));
+      //Pages.loginMenu.action();
       Account.backupAll();
-    }
-    catch (Exception e) {
+      l.setText("loaded");
+    } catch (Exception e) {
       e.printStackTrace();
     }
   }
