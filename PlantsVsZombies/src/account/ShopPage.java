cc09f6a336638fbedaef38b2a5a27e5c832131e1
HKalbasi <kalbasi@ce.sharif.edu>
2020-01-28 19:56:51 +0330

    feat: add base of shop page

--- PlantsVsZombies/src/account/ShopPage.java
+++ PlantsVsZombies/src/account/ShopPage.java
@@ -17,29 +17,8 @@ public class ShopPage implements Page<Unit> {
 
   @Override
   public Effect<Unit> action() {
-    return Effect.syncWork(()->{
-      Store me = Account.current.store;
-      while (true) {
-        Program.clearScreen();
-        System.out.println("Enter number to buy ( or e for exit )");
-        System.out.println("Your money is: " + me.money + "$");
-        int i = 0;
-        ArrayList<BeingDna> buyOptions = new ArrayList<>();
-        List<BeingDna> allDna = new ArrayList<>();
-        allDna.addAll(PlantDna.getAllDnas());
-        allDna.addAll(ZombieDna.getAllDnas());
-        for (BeingDna x : allDna) {
-          if (me.haveCard(x))
-            System.out.println("X. " + x.getName() + "     sold out");
-          else if (me.money < x.getShopPrice()) {
-            System.out.println("X. " + x.getName() + "     " + ConsoleColors.red(x.getShopPrice() + "$"));
-          } else {
-            System.out.println(i + ". " + x.getName() + "     " + x.getShopPrice() + "$");
-            buyOptions.add(x);
-            i++;
-          }
-        }  
-      }
+    return new Effect<>(h->{
+
     });
   }
 }
