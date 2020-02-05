fef1a43b6319281a76209de7dfe0f042ae4b965a
HKalbasi <kalbasi@ce.sharif.edu>
2020-02-05 19:56:39 +0330

    feat: add money to users for test propose

--- PlantsVsZombies/src/account/Store.java
+++ PlantsVsZombies/src/account/Store.java
@@ -15,7 +15,7 @@ public class Store implements Serializable {
   private static final long serialVersionUID = 5608599943348289833L;
   // private Account owner;
   private Set<String> myCard = new HashSet<>();
-    public int money = 0;
+    public int money = 10000;
   public void buyCard(BeingDna card) {
     myCard.add(card.getName());
     money -= card.getShopPrice();
