c94f4575ca86fcc6dd94dedf41238cce3f563b92
HKalbasi <kalbasi@ce.sharif.edu>
2020-02-06 21:50:18 +0330

    feat: add join zombie player

--- PlantsVsZombies/src/game/pvp/PvpGame.java
+++ PlantsVsZombies/src/game/pvp/PvpGame.java
@@ -1,7 +1,10 @@
 package game.pvp;
 
 import account.Account;
+import account.AccountForm;
+import client.Client;
 import creature.being.plant.PlantDna;
+import creature.being.zombie.ZombieDna;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
@@ -10,8 +13,11 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import main.Program;
+import page.Collection;
+import util.*;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class PvpGame {
     private static final Integer randomizer = 3865;
@@ -21,6 +27,7 @@ public class PvpGame {
     private final String plant;
     private final String zombie;
     private ArrayList<String> plantHand = new ArrayList<>();
+    private ArrayList<String> zombieHand = new ArrayList<>();
 
     private ArrayList<ArrayList<String>> moves = new ArrayList<>();
 
@@ -62,7 +69,7 @@ public class PvpGame {
             if (gameState.equals("WAITING_TO_JOIN")) {
                 if (zombie.equals(username)) {
                     button.setText("join");
-                    // TODO:
+                    button.setOnMouseClicked(e->joinZombie(id).execute());
                 } else {
                     button.setText("waiting");
                 }
@@ -78,6 +85,26 @@ public class PvpGame {
 
         hBox.getChildren().addAll(text, button);
         return hBox;
+    }
+
+    private static Effect<Unit> joinZombie(String id) {
+        return new Collection<ZombieDna>(Account::getCurrentUserZombies, 7)
+            .action()
+            .map(x -> Account.myToken+"\n"+id+
+                "\n"+x.stream().map(y -> y.getName())
+                .collect(Collectors.joining("\n"))+"\n"
+            )
+            .flatMap(x -> Client.get("pvp/join", x))
+            .show();
+    }
+
+    private static PvpGame findGameById(String id) {
+        Integer gameId = Integer.parseInt(id);
+        for (PvpGame game : allIds)
+            if (gameId.equals(game.id)) {
+                return game;
+            }
+        return null;
     }
 
     // body's first argument is username !!!
@@ -106,6 +133,16 @@ public class PvpGame {
                 game.plantHand.add(body.get(i));
             }
             return game.id+"";
+        }
+        if (url.equals("join")) {
+            Account user = Account.getByToken(body.get(0));
+            if (user == null) return "FAIL401";      
+            PvpGame game = findGameById(body.get(1));
+            for (int i = 2; i < body.size(); i++) {
+                game.zombieHand.add(body.get(i));
+            }
+            game.gameState = GameState.RUNNING_PLANTS;
+            return "OK";
         }
         if (url.equals("cancel")) {
             Integer gameId = Integer.parseInt(body.get(1));
