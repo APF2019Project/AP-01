b63fd6264e3217f0d96c5ce1c1846543134baa94
HKalbasi <kalbasi@ce.sharif.edu>
2020-02-07 16:44:18 +0330

    feat: rename

--- PlantsVsZombies/src/server/AccountHandler.java
+++ PlantsVsZombies/src/server/AccountHandler.java
@@ -15,6 +15,22 @@ public class AccountHandler {
       user.removeToken();
       return "OK";
     }
+    if (url.equals("delete")) {
+      Account user = Account.getByToken(body.get(0));
+      if (user == null) return "FAIL401";
+      user.delete();
+      return "OK";
+    }
+    if (url.equals("rename")) {
+      Account user = Account.getByToken(body.get(0));
+      if (user == null) return "FAIL401";
+      try{
+        user.rename(body.get(1));
+      }
+      catch(Throwable e) {
+        return "FAIL";
+      }
+    }
     return "404";
   }
 }
